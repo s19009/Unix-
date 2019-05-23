40bae9cd7e4b68399d480d69f46453690933f153
s19009 <s19009@std.it-college.ac.jp>
2019-05-23 12:55:37 +0900

    5/23課題

---
+++ 0523/052305.sh
@@ -0,0 +1,5 @@
+#find /etc -type f 2> /dev/null | xargs grep -L '172.16.40.1' 2> /dev/null 
+
+#grep -rL '172.16.40.1' /etc 2> /dev/null
+
+find /etc/ -type f -print0 2> /dev/null |xargs -0 grep -L '172.16.40.1'
