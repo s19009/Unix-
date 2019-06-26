cefad1433979623ba933c4c7e840b3a7e9c7193a
s19009 <s19009@std.it-college.ac.jp>
2019-06-26 12:28:06 +0900

    5/23訂正

--- 0523/052305.sh
+++ 0523/052305.sh
@@ -2,4 +2,4 @@
 
 #grep -rL '172.16.40.1' /etc 2> /dev/null
 
-find /etc/ -type f -print0 2> /dev/null |xargs -0 grep -L '172.16.40.1'
+find /etc/ -type f -print0 2> /dev/null |xargs -0 grep -l '172.16.40.1' 2> /dev/null
